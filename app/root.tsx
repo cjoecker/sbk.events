605fb4bbd9f866faaeb0d6a2e7bcb1bc39fd7dbf
Christian Jöcker <c.jocker@hotmail.com>
2024-11-18 22:33:03 +0100

    add coming soon information

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-black">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,19 @@ import type { MetaFunction } from "@netlify/remix-runtime";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "SBK Events" },
+    { name: "description", content: "salsa bachata and kizomba events" },
+    { name: "keywords", content: "salsa events, bachata events, kizomba events, salsa dance, bachata dance, kizomba dance, salsa festivals, bachata festivals, kizomba festivals, salsa parties, bachata parties, kizomba parties, salsa workshops, bachata workshops, kizomba workshops" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex h-screen v-screem" style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <div className="m-auto text-center flex flex-col gap-2">
+      <h1 className="text-white text-3xl">Worldwide salsa, bachata and kizomba events!</h1>
+      <h2 className="text-gray-400 text-2xl m-auto">Coming soon...</h2>
+      </div>
     </div>
   );
 }
