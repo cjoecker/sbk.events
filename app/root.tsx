da8f80cb951aef07f5426325989d70d7bf466b90
Christian Jöcker <c.jocker@hotmail.com>
2024-11-21 18:07:32 +0100

    add favicons

--- app/root.tsx
+++ app/root.tsx
@@ -82,6 +82,31 @@ export const links: LinksFunction = () => {
 			type: "font/woff",
 			crossOrigin: "anonymous",
 		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "32x32",
+			href: "/favicon.png",
+		},
+		{
+			rel: "apple-touch-icon",
+			sizes: "180x180",
+			href: "/favicons/apple-touch-icon.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "32x32",
+			href: "/favicons/favicon-32x32.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "16x16",
+			href: "/favicons/favicon-16x16.png",
+		},
+		{ rel: "manifest", href: "/site.webmanifest" },
+		{ rel: "icon", href: "/favicon.ico" },
 	];
 };
 
