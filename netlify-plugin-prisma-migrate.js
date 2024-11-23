9dbb63869054a5d7348e2b7d2f20eb3d2706f3bf
Christian Jöcker <c.jocker@hotmail.com>
2024-11-23 13:36:50 +0100

    fix netlify plugin manifest

--- netlify-plugin-prisma-migrate.js
+++ netlify-plugin-prisma-migrate.js
@@ -1,11 +1,12 @@
 import { NetlifyPlugin } from '@netlify/build';
 
-export const onPreBuild: NetlifyPlugin['onPreBuild'] = async ({ utils }) => {
+
+export const onPreBuild = async ({ utils }) => {
 	try {
 		console.log("Running Prisma migrations...");
 		await utils.run.command("npx prisma migrate deploy");
 		console.log("Prisma migrations completed.");
 	} catch (error) {
-		utils.build.failBuild("Failed to run Prisma migrations", { error } as never);
+		utils.build.failBuild("Failed to run Prisma migrations", { error });
 	}
 }
