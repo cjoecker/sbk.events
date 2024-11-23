21eca90de2b62a57939130e1828928da6869a9a6
Christian Jöcker <c.jocker@hotmail.com>
2024-11-23 13:13:38 +0100

    add netlify migrate prisma

---
+++ netlify-plugin-prisma-migrate.js
@@ -0,0 +1,11 @@
+module.exports = {
+	onPreBuild: async ({ utils }) => {
+		try {
+			console.log("Running Prisma migrations...");
+			await utils.run.command("npx prisma migrate deploy");
+			console.log("Prisma migrations completed.");
+		} catch (error) {
+			utils.build.failBuild("Failed to run Prisma migrations", { error });
+		}
+	},
+};
