2d9c9693d5bbb7728c918283d407018dda4e271b
Christian Jöcker <c.jocker@hotmail.com>
2024-11-24 12:50:39 +0100

    fix migration

--- prisma/migrations/20241123160610_add_likes/migration.sql
+++ prisma/migrations/20241123160610_add_likes/migration.sql
@@ -5,4 +5,7 @@
 
 */
 -- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "likes" INTEGER NOT NULL;
+ALTER TABLE "Event" ADD COLUMN "likes" INTEGER NOT NULL DEFAULT 0;
+
+-- Update existing rows to set the default value
+UPDATE "Event" SET "likes" = 0 WHERE "likes" IS NULL;
