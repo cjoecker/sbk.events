da03432dd3ceab56b87a6513e61d19c7df80cd76
Christian Jöcker <c.jocker@hotmail.com>
2024-11-25 17:23:20 +0100

    add sitemap

--- app/utils/remix.ts
+++ app/utils/remix.ts
@@ -5,3 +5,12 @@ import { json as DeprecatedJSON } from "@remix-run/node";
 
 // eslint-disable-next-line @typescript-eslint/no-deprecated, unicorn/prefer-export-from
 export const json = DeprecatedJSON;
+
+export function getDomainUrl(request: Request) {
+	const host =
+		request.headers.get('X-Forwarded-Host') ??
+		request.headers.get('host') ??
+		new URL(request.url).host
+	const protocol = request.headers.get('X-Forwarded-Proto') ?? 'http'
+	return `${protocol}://${host}`
+}
