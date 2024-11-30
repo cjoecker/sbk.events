0dd8c87ab3c6ef335ed18ca121b07ea0890a04a6
Christian Jöcker <c.jocker@hotmail.com>
2024-11-30 09:48:16 +0100

    fix login form

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,14 +1,8 @@
 import { SEOHandle } from "@nasa-gcn/remix-seo";
 import { Button } from "@nextui-org/react";
 import { ActionFunctionArgs, redirect } from "@remix-run/node";
-import { useForm } from "@rvf/remix";
-import { validationError } from "@rvf/remix";
-import { withZod } from "@rvf/zod";
 import React from "react";
-import { useTranslation } from "react-i18next";
-import { z } from "zod";
 
-import { Input } from "~/components/rvf/input";
 import { getSession } from "~/modules/session.server";
 import { json } from "~/utils/remix";
 
@@ -18,20 +12,10 @@ export const handle: SEOHandle = {
 	},
 };
 
-const validator = withZod(
-	z.object({
-		key: z.string().min(1),
-	})
-);
-
 export const action = async ({ request }: ActionFunctionArgs) => {
-	const result = await validator.validate(await request.formData());
-	if (result.error) {
-		return validationError(result.error, result.submittedData);
-	}
-
+	const formData = await request.formData();
+	const key = formData.get("key") as string | undefined;
 	const { setIsAdmin, getHeaders } = await getSession(request);
-	const { key } = result.data;
 	if (key === process.env.CRUD_EVENT_KEY) {
 		setIsAdmin();
 		return redirect("/events", {
@@ -44,16 +28,10 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 };
 
 export default function Login() {
-	const { t } = useTranslation();
-	const form = useForm({
-		method: "post",
-		validator,
-	});
-
 	return (
-		<form className="flex flex-col gap-2" {...form.getFormProps()}>
-			<Input label={t("key")} scope={form.scope("key")} type={"password"} />
-			<Button type="submit">{t("send")}</Button>
+		<form className="flex flex-col gap-2" method={"post"} action={"/login"}>
+			<input name="key" type="password" placeholder={"key"} />
+			<Button type="submit">Login</Button>
 		</form>
 	);
 }
