9bda07c6c8a7d0b2fbf47264d6cf659906f3364c
Christian Jöcker <46030630+cjoecker@users.noreply.github.com>
2024-12-02 08:40:19 +0100

    add vercel (#17)
    * add prisma generate
    
    * remove netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import { envOnlyMacros } from "vite-env-only";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/remix/vite";
+
 
 export default defineConfig({
-	plugins: [envOnlyMacros(), remix(), netlifyPlugin(), tsconfigPaths()],
+	plugins: [envOnlyMacros(), remix({
+		presets: [vercelPreset()],
+	}), tsconfigPaths()],
 	server: { port: 4000 },
 });
