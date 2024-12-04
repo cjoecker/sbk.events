373c771df7e10496f82dcb498bcc4d82b96a8ea2
Christian Jöcker <c.jocker@hotmail.com>
2024-12-04 16:03:14 +0100

    add speed insights

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -6,10 +6,14 @@ import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import { getInitialNamespaces } from "remix-i18next/client";
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+
 
 import * as i18n from "~/config/i18n";
 
 inject();
+injectSpeedInsights();
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 i18next
