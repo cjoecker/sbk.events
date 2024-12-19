5c99b654bd2a2f2390cdcc9c045fcd24787098ea
Christian Jöcker <c.jocker@hotmail.com>
2024-12-19 20:38:46 +0100

    add blog create page

--- app/components/blog-post.tsx
+++ app/components/blog-post.tsx
@@ -2,16 +2,19 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 export type BlogPostProps = {
-	title: string;
-	content: string;
+	title: string | undefined;
+	content: string | undefined;
 	date: string;
 };
-export const BlogPost = ({title,content,date}: BlogPostProps) => {
+export const BlogPost = ({ title, content, date }: BlogPostProps) => {
+	const formattedDate = new Date(date).toLocaleDateString();
 	return (
-		<div>
-			<h1>{title}</h1>
-			<p>{date}</p>
-			<div><Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown></div>
+		<div className="glass-l-black p-2">
+			<h1 className="text-3xl font-bold mt-1">{title}</h1>
+			<div className="text-md text-gray-300">{formattedDate}</div>
+			<div className="mt-4">
+				<Markdown className="markdown" remarkPlugins={[remarkGfm]}>{content}</Markdown>
+			</div>
 		</div>
 	);
 };
