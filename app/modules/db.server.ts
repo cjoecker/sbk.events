98582d68b6628907b1c8746a5233009d7080ded4
Christian Jöcker <c.jocker@hotmail.com>
2024-12-27 20:32:52 +0100

    add local database

--- app/modules/db.server.ts
+++ app/modules/db.server.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 export const db = remember("prisma", () => {
-	const client = new PrismaClient().$extends(withAccelerate());
+	const client = new PrismaClient()
+	if(process.env.NODE_ENV === "production") {
+		client.$extends(withAccelerate());
+	}
 	void client.$connect();
 	return client;
 });
