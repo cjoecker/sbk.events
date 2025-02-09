c89198b2fba56c8996461c1d2b0380d33d0ec39c
Christian Jöcker <c.jocker@hotmail.com>
2025-02-09 19:16:22 +0100

    refactor prompt

--- app/routes/api.ai-events.ts
+++ app/routes/api.ai-events.ts
@@ -22,101 +22,14 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 		.object({
 			details: z.string().optional(),
 			image: z.string().optional(),
-			infoUrl: z.string().optional(),
+			infoUrl: z.string(),
 		})
 		.parse(body);
 
-	let socialMediaMessage = "";
-	if (data.details) {
-		socialMediaMessage = `Social media message: ${data.details} \n`;
-	}
-
-	if (!(await isSocialEvent(socialMediaMessage, data.image))) {
-		const error =
-			"The message and the image are not about a social dancing event";
-		console.error(error);
-		return {
-			error,
-		};
-	}
-
-	const organizers = await db.organizer.findMany({
-		where: {
-			events: {
-				some: {
-					location: {
-						city: {
-							name: CITY,
-						},
-					},
-				},
-			},
-		},
-		select: { name: true },
-	});
-	const organizerNames = organizers
-		.map((organizer) => {
-			return `${organizer.name} \n`;
-		})
-		.join("");
-
-	const locations = await db.location.findMany({
-		where: {
-			city: {
-				name: CITY,
-			},
-		},
-		select: { name: true },
-	});
-	const locationNames = locations
-		.map((location) => {
-			return `${location.name} \n`;
-		})
-		.join("");
-	const futureEvents = await db.event.findMany({
-		where: {
-			startDate: {
-				gte: new Date(),
-			},
-			location: {
-				city: {
-					name: CITY,
-				},
-			},
-		},
-		select: { organizer: true, startDate: true },
-	});
-	const futureEventNames = futureEvents
-		.map((event) => {
-			return `${format(event.startDate, "dd/MM/yy hh:mm")} - ${
-				event.organizer.name
-			} \n`;
-		})
-		.join("");
-
-	const messageParameters = {
-		organizerNames,
-		locationNames,
-		futureEventNames,
-		socialMediaMessage,
-	};
-
-	const content: Content[] = [
-		{
-			type: "text",
-			text: getMessageWithParameters(getEventPrompt, messageParameters),
-		},
-	];
-
-	if (data.image) {
-		content.push({
-			type: "image",
-			image: data.image,
-		});
-	}
+	const content = await getMessagesContent(data.details, data.image);
 
 	const { object, usage } = await generateObject({
-		model: openai("gpt-4o-mini"),
+		model: openai(MODEL_NAME),
 		schema: aiEventSchema,
 		messages: [
 			{
@@ -126,110 +39,97 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 		],
 	});
 
-	if (await eventExists(object.date, object.startTime, object.locationName)) {
-		const error = `Event from ${object.organizerName} at ${object.date} already exists`;
+	if (object.confidenceLevel < 50) {
+		const error = `The confidence level of ${object.confidenceLevel} is too low`;
 		console.error(error);
 		return {
 			error,
 		};
 	}
-
-	const savedEvent = await saveEvent(object);
-
-	// console.info(JSON.stringify(object, null, 2));
-	const price =
-		usage.promptTokens * promptTokensPrice +
-		usage.completionTokens * completionTokensPrice;
-	console.info("Price: $", price);
-
-	return savedEvent;
-};
-
-async function isSocialEvent(
-	socialMediaMessage: string,
-	image: string | undefined
-) {
-	const content: Content[] = [
-		{
-			type: "text",
-			text: getMessageWithParameters(checkIfEventPrompt, {
-				socialMediaMessage,
-			}),
-		},
-	];
-
-	if (image) {
-		content.push({
-			type: "image",
-			image,
-		});
+	const { alreadyExists, isSetToPublished } = await eventAlreadyExists(
+		object.date,
+		object.startTime,
+		object.locationName
+	);
+	if (alreadyExists) {
+		const error = `Event from ${object.organizerName} at ${object.date} already exists`;
+		console.info(error);
+		return {
+			alreadyExists,
+			isSetToPublished,
+		};
 	}
 
-	const { object } = await generateObject({
-		model: openai("gpt-4o-mini"),
-		schema: z.object({
-			// infoUrl: z.string().trim().url(),
-			isSocialEvent: z.boolean(),
-		}),
-		messages: [
-			{
-				role: "user",
-				content: content,
-			},
-		],
-	});
-	return object.isSocialEvent;
-}
+	const savedEvent = await saveEvent(object, data.infoUrl);
 
-const checkIfEventPrompt = `
-You are a maintainer of salsa, bachata and kizomba dancing social events.
-Your task is to identify if the message and the image are about a social event.
-The message can only contain information about a single event for one day.
-The information cannot be about a congress or festival.
+	const price =
+		usage.promptTokens * PROMPT_TOKENS_PRICE +
+		usage.completionTokens * RESPONSE_TOKENS_PRICE;
+	console.info("Price: $", price);
 
-{{ socialMediaMessage }}
-`;
+	return {
+		event: savedEvent,
+		price,
+		confidenceLevel: object.confidenceLevel,
+	};
+};
 
-const getEventPrompt = `
-You are a maintainer of salsa, bachata and kizomba dancing social events.
+const PROMPT_TOKENS_PRICE = 0.15 / 1_000_000;
+const RESPONSE_TOKENS_PRICE = 0.6 / 1_000_000;
+const MODEL_NAME = "gpt-4o-mini";
+
+const GET_EVENT_PROMPT = `
+You are an expert maintainer of salsa, bachata and kizomba dancing social events.
 Socials are events for no more than one day where the people can dance.
-Your task is to generate an event objects that can be saved in the database so that the people know where to dance.
-The messages are from social media might contain unnecessary information.
-We are in the year ${new Date().getFullYear()}
+Your goal is to generate an event data objects that can be saved in the database so that the people know where to dance.
 
+For your task, you should generate an event object following this guide:
 
-If there is no end time, assume the event ends at 3:00 if it starts at night.
-If it starts in the morning or evening, assume it ends 3 hours after the last activity mentioned or the start time.
-If there is the time for the workshops/classes, give me the as start time the time for the party.
-Give me the date as YYYY-MM-DD. The time as HH:MM.
+1. **Confidence Level**:
+- The confidence level from 0 to 100 is the probability that the information is correct and it is a real social event.
+- The image and message you get might or might not be related to a social event. It can be a random image.
+- The message should only contain information about a single event for one day. If not, the confidence level should be lower than 50.
+- The information cannot be about a congress or festival. It must be a social event.
+- The event might exist already. If it does, you should return a confidence level lower than 50. 
+This are the future events already in the database:
+{{ futureEventNames }}
 
-The salsa, bachata and kizomba percentages must be in total 100.
-In case there is no information about the percentages, assume it from the title of the event.
+2. **Date, start and end time**: 
+- We are in the year ${new Date().getFullYear()} 
+- If there is no end time, assume the event ends at 3:00 if it starts at night.
+- If the event starts in the morning or evening, assume it ends 3 hours after the last activity mentioned or the start time.
+- If there is the time for the workshops/classes, give me the as start time the time for the party.
+- Give the date as YYYY-MM-DD. 
+- Give the time as HH:MM.
+
+2. **Salsa, bachata and kizomba percentages**: 
+- This is the percentage of the music style that is played in the event. 
+- The salsa, bachata and kizomba percentages must be in total 100.
+- In case there is no exact information about the percentages, assume it from the message or image.
 E.g. if the title says something like "Salsa Something", you can assume 100% salsa.
-In case it cannot be assumed from the title, assume 40% salsa, 60% bachata, 0% kizomba.
-Give me the event name in Title Clase.
+- In case the percentages cannot be assumed, assume 40% salsa, 60% bachata, 0% kizomba.
 
-Prefer the organizers names as they are saved in the database.
-If they are not saved, is ok to add a give me a new organizer that is not in the list.
-If you cannot recognize the organizer, use the location name as organizer. 
-This is the organizers already saved in the database:
+3. **Salsa, bachata and kizomba percentages**: 
+- The event name should be in Title Clase.
+
+4. **Organize Name**:
+- Prefer the organizers names as they are saved in the database.
+- If the organizer you recognized is not in the list, is ok to return a new organizer that is not in the list.
+- If you cannot recognize the organizer, use the location name as organizer. 
+- This is the organizers already saved in the database:
 {{ organizerNames }}
 
-Prefer the locations names as they are saved in the database.
-If they are not saved, is ok to add a give me a new location that is not in the list.
-This are the locations area already saved in the database:
+5. **Organize Name**:
+- Prefer the locations names as they are saved in the database.
+- If the location you recognized is not in the list, is ok to return a new organizer that is not in the list.
+- This are the locations area already saved in the database:
 {{ locationNames }}
-
-The events might already exist in the database.
-If the event already exists, throw an error.
-This are the future events already saved in the database:
-{{ futureEventNames }}
 
 {{ socialMediaMessage }}
 `;
 
 const aiEventSchema = z.object({
-	// infoUrl: z.string().trim().url(),
+	confidenceLevel: z.number().min(0).max(100),
 	name: z.string().trim().min(1),
 	organizerName: z.string().min(1),
 	date: z.string(),
@@ -242,46 +142,7 @@ const aiEventSchema = z.object({
 });
 type AiEvent = z.infer<typeof aiEventSchema>;
 
-function getMessageWithParameters(
-	template: string,
-	parameters: Record<string, string>
-) {
-	return template.replaceAll(/{{\s*(\w+)\s*}}/g, (match, key: string) => {
-		return parameters[key] ?? match;
-	});
-}
-
-async function eventExists(date: string, time: string, locationName: string) {
-	const startDate = new Date(`${date}T${time}Z`);
-	const event = await db.event.findFirst({
-		where: {
-			startDate,
-			location: {
-				name: {
-					equals: locationName.trim(),
-					mode: "insensitive",
-				},
-			},
-		},
-		select: {
-			status: true,
-			id: true,
-		},
-	});
-	const exists = !!event;
-	if (exists && event.status !== EventStatus.PUBLISHED) {
-		await db.event.update({
-			where: { id: event.id },
-			data: { status: EventStatus.PUBLISHED },
-		});
-		console.info(
-			`Event from ${locationName} at ${date} was not published. Now it is published`
-		);
-	}
-	return !!event;
-}
-
-async function saveEvent(aiEvent: AiEvent) {
+async function saveEvent(aiEvent: AiEvent, infoUrl: string) {
 	const city = await db.city.findFirst({
 		where: { name: CITY },
 		select: { id: true },
@@ -312,9 +173,9 @@ async function saveEvent(aiEvent: AiEvent) {
 		select: { id: true },
 	});
 
-	return await db.event.create({
+	return db.event.create({
 		data: {
-			infoUrl: "",
+			infoUrl: infoUrl,
 			name: aiEvent.name,
 			startDate: startDate,
 			endDate: endDate,
@@ -344,16 +205,177 @@ async function saveEvent(aiEvent: AiEvent) {
 			kizombaPercentage: aiEvent.kizombaPercentage,
 			status: EventStatus.PUBLISHED,
 		},
+		select: {
+			id: true,
+			infoUrl: true,
+			name: true,
+			startDate: true,
+			endDate: true,
+			location: {
+				select: {
+					name: true,
+					googleMapsUrl: true,
+				},
+			},
+			organizer: {
+				select: {
+					name: true,
+				},
+			},
+			salsaPercentage: true,
+			bachataPercentage: true,
+			kizombaPercentage: true,
+		},
 	});
 }
 
-const promptTokensPrice = 0.15 / 1_000_000;
-const completionTokensPrice = 0.6 / 1_000_000;
+async function eventAlreadyExists(
+	date: string,
+	time: string,
+	locationName: string
+) {
+	const response = {
+		alreadyExists: false,
+		isSetToPublished: false,
+	};
+	const startDate = new Date(`${date}T${time}Z`);
+	const event = await db.event.findFirst({
+		where: {
+			startDate,
+			location: {
+				name: {
+					equals: locationName.trim(),
+					mode: "insensitive",
+				},
+			},
+		},
+		select: {
+			status: true,
+			id: true,
+		},
+	});
+	const exists = !!event;
+	if (exists && event.status !== EventStatus.PUBLISHED) {
+		await db.event.update({
+			where: { id: event.id },
+			data: { status: EventStatus.PUBLISHED },
+		});
+		response.isSetToPublished = true;
+		console.info(
+			`Event from ${locationName} at ${date} was not published. Now it is published`
+		);
+	}
+	response.alreadyExists = exists;
+
+	return response;
+}
+
+async function getOrganizerNames() {
+	const organizers = await db.organizer.findMany({
+		where: {
+			events: {
+				some: {
+					location: {
+						city: {
+							name: CITY,
+						},
+					},
+				},
+			},
+		},
+		select: { name: true },
+	});
+	return organizers
+		.map((organizer) => {
+			return `${organizer.name} \n`;
+		})
+		.join("");
+}
+
+async function getLocationNames() {
+	const locations = await db.location.findMany({
+		where: {
+			city: {
+				name: CITY,
+			},
+		},
+		select: { name: true },
+	});
+	return locations
+		.map((location) => {
+			return `${location.name} \n`;
+		})
+		.join("");
+}
+
+async function getFutureEventNames() {
+	const futureEvents = await db.event.findMany({
+		where: {
+			startDate: {
+				gte: new Date(),
+			},
+			location: {
+				city: {
+					name: CITY,
+				},
+			},
+		},
+		select: { organizer: true, startDate: true },
+	});
+	return futureEvents
+		.map((event) => {
+			return `${format(event.startDate, "dd/MM/yy hh:mm")} - ${
+				event.organizer.name
+			} \n`;
+		})
+		.join("");
+}
+
+async function getMessagesContent(
+	details: string | undefined,
+	image: string | undefined
+) {
+	let socialMediaMessage = "";
+	if (details) {
+		socialMediaMessage = `SOCIAL MEDIA MESSAGE: ${details} \n`;
+	}
+
+	const organizerNames = await getOrganizerNames();
+	const locationNames = await getLocationNames();
+	const futureEventNames = await getFutureEventNames();
+
+	const messageParameters = {
+		organizerNames,
+		locationNames,
+		futureEventNames,
+		socialMediaMessage,
+	};
+
+	const content: Content[] = [
+		{
+			type: "text",
+			text: getMessageWithParameters(GET_EVENT_PROMPT, messageParameters),
+		},
+	];
+
+	if (image) {
+		content.push({
+			type: "image",
+			image: image,
+		});
+	}
+	return content;
+}
+
+function getMessageWithParameters(
+	template: string,
+	parameters: Record<string, string>
+) {
+	return template.replaceAll(/{{\s*(\w+)\s*}}/g, (match, key: string) => {
+		return parameters[key] ?? match;
+	});
+}
 
 type Content =
 	| { type: "text"; text: string }
 	| { type: "image"; image: string };
-
-// TODO
-// add cached input
-// https://sdk.vercel.ai/providers/ai-sdk-providers/openai#prompt-caching
